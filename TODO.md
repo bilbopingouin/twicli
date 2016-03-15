12731444746ba23c51a3002b9929b31984771763
bilbopingouin <tudi.lebeis@gmail.com>
2016-03-15 09:33:02 +0000

    some clean up

--- TODO.md
+++ TODO.md
@@ -5,4 +5,4 @@
 - Implement a ncurses display
 - Exception handling
 - Upgrade with option to write tweets
-- Colour enhancing: get it platform indep: https://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python
+- Colour enhancing: get it platform indep: https://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python => DONE
--- read_tweets.py
+++ read_tweets.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python
-# https://codereview.stackexchange.com/questions/122449/cli-twitter-client-in-python
-
-# print is a function print() and python3 does not allow mix of tabs and spaces for indentation
 
 import sys
 import os
@@ -16,8 +13,8 @@ dir_creds = "./data/oauth/"
 
 # Colorama: init() for windows parsing
 colorama.init()
-# for n in range(100):
-#     print n, '\033['+str(n)+'m'+"whatever"+'\033[0m'
+
+# Definitions of some colours
 col_bgred   = colorama.Back.RED
 col_bgblue  = colorama.Back.BLUE
 
@@ -31,17 +28,17 @@ col_fggrey  = '\x1b[90m' # this is grey
 col_end     = colorama.Style.RESET_ALL
 
 
-
+# Argument(s) parsing
 def main(argv):
     global def_counts, users
 
     user = ''
     
-    parser = argparse.ArgumentParser(description='twicli Twitter App')
-    parser.add_argument('-c', '--counts', help='Count of tweets to retrieve.',  required=False, action="store", dest="def_counts", type=int)
-    parser.add_argument('-u', '--user',   help='Username',	                required=False, action="store", dest="user")
-    parser.add_argument('-a', '--all',    help='All Users',	                required=False, action="store_true")
-    parser.add_argument('-l', '--list',   help='List available users',          required=False, action="store_true")
+    parser = argparse.ArgumentParser(description=col_bold+'twicli Twitter App'+col_end)
+    parser.add_argument('-c', '--counts', help='Count of tweets to retrieve.',  required=False, action='store', dest='def_counts', type=int)
+    parser.add_argument('-u', '--user',   help='Username',	                required=False, action='store', dest='user')
+    parser.add_argument('-a', '--all',    help='All Users',	                required=False, action='store_true')
+    parser.add_argument('-l', '--list',   help='List available users',          required=False, action='store_true')
     try:
         options = parser.parse_args()
     except:
@@ -52,11 +49,11 @@ def main(argv):
         users.append(user)
 
     if options.all or options.list:
-        print(col_fgred + "All users:" + col_end)
+        print(col_fgred + 'All users:' + col_end)
         if os.path.exists(os.path.expanduser(dir_creds)):
             token_files = filter(lambda x: x.endswith('.token'), os.listdir(os.path.expanduser(dir_creds)))
             for f in token_files:
-                usr = f[:f.index(".token")]
+                usr = f[:f.index('.token')]
                 if options.all:
                     users.append(usr)
                 else:
@@ -64,37 +61,41 @@ def main(argv):
             if options.list:
                 exit(0)
         else:
-            print(col_bgblue + "No user to be added, path undefined" + col_end)
+            print(col_bgblue + 'No user to be added, path undefined' + col_end)
             exit(2)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main(sys.argv[1:])
 
 if len(users) < 1:
-    users.append("bilbo_pingouin")
+    users.append('bilbo_pingouin')
 
 
 for user in users:
     print ("\n" + col_bgred + col_bold + user + col_end)
-# Retrieve the credentials for a given account
-    if not os.path.exists(dir_creds):
-        os.makedirs(dir_creds) # I just assume there is not race issue here!
-
-    file_creds = dir_creds + user + ".token"
-    MY_TWITTER_CREDS = os.path.expanduser(file_creds)
-
-    #TODO: try to setup a page on my server where those values could be obtained!
-    api_token_file = "data/api_token.dat"
+    
+    # API Token
+    api_token_file = 'data/api_token.dat'
     if os.path.exists(api_token_file):
         cust_token, cust_secret = read_token_file(os.path.expanduser(api_token_file))
     else:
-        print("ERROR: The app is not identified!")
+        print(col_bgred+'ERROR: The app is not identified!'+col_end)
+        
+    # User credentials directory
+    if not os.path.exists(dir_creds):
+        os.makedirs(dir_creds) # I just assume there is not race issue here!
 
+    # User credentials file
+    file_creds = dir_creds + user + '.token'
+    MY_TWITTER_CREDS = os.path.expanduser(file_creds)
+
+    # Adding the credentials if they are not already there
     if not os.path.exists(MY_TWITTER_CREDS):
-        oauth_dance("Twit on CLI", cust_token, cust_secret,
+        oauth_dance('Twit on CLI', cust_token, cust_secret,
                     MY_TWITTER_CREDS)
 
+    # Getting tokens for oauth
     oauth_token, oauth_secret = read_token_file(MY_TWITTER_CREDS)
 
     # OAuth idnetification
